31e1b03b891e8775dccb28953cbf81961d1c0a6c
SymmetricChaos <symmetricfractal@gmail.com>
2026-08-12 19:11:16 -0400

    more pa

--- src/preference_assessment/pa_page.rs
+++ src/preference_assessment/pa_page.rs
@@ -1,5 +1,11 @@
-use crate::{app::DataPro, ui_elements::DataProUiElements};
-use egui::Ui;
+use crate::{
+    app::DataPro,
+    ui_elements::DataProUiElements,
+    utils::{overwrite_file, windows_error_dialog},
+};
+use egui::{RichText, Ui};
+use egui_extras::Column;
+use egui_file_dialog::FileDialog;
 use itertools::Itertools;
 use rand::{rngs::StdRng, seq::SliceRandom};
 use std::fmt::Display;
@@ -20,61 +26,150 @@ impl Display for PaType {
     }
 }
 
-#[derive(Default)]
+// example conditions
+// teddy bear
+// drum kit
+// book
+// iPad
+// rattle
+// ball
+// animal toys
+// trampoline
+
 pub struct PreferenceAssessment {
-    pub patype: PaType,
     pub conditions_string: String,
     pub conditions: Vec<String>,
     pub all_pairs: Vec<(String, String)>,
+    pub ordered: bool,
+    pub file_dialog: FileDialog,
+}
+
+impl Default for PreferenceAssessment {
+    fn default() -> Self {
+        let fd = FileDialog::default().default_file_name("preferences.txt");
+
+        Self {
+            conditions_string: Default::default(),
+            conditions: Default::default(),
+            all_pairs: Default::default(),
+            ordered: true,
+            file_dialog: fd,
+        }
+    }
 }
 
 impl PreferenceAssessment {
+    pub fn update(&mut self) {
+        self.update_conditions();
+        self.update_pairs();
+    }
+
     pub fn update_conditions(&mut self) {
         self.conditions = self
             .conditions_string
-            .split(",")
+            .split("\n")
             .map(|s| s.trim().to_string())
             .filter(|s| !s.is_empty())
             .collect()
     }
 
-    pub fn update_pairs(&mut self, rng: &mut StdRng) {
+    pub fn update_pairs(&mut self) {
         self.all_pairs.clear();
-        for a in self.conditions.iter() {
-            for b in self.conditions.iter() {
+        for (i, a) in self.conditions.iter().enumerate() {
+            for (j, b) in self.conditions.iter().enumerate() {
+                if !self.ordered {
+                    if i > j {
+                        continue;
+                    }
+                }
                 if a != b {
                     self.all_pairs.push((a.clone(), b.clone()));
                 }
             }
         }
+    }
+
+    pub fn shuffle_pairs(&mut self, rng: &mut StdRng) {
         self.all_pairs.shuffle(rng);
     }
 
     pub fn view(app: &mut DataPro, ui: &mut Ui) {
-        egui::CentralPanel::default().show(ui, |ui| {
+        app.preference_assessment.file_dialog.update(ui.ctx());
+        if let Some(path) = app.preference_assessment.file_dialog.take_picked() {
+            let data = app
+                .preference_assessment
+                .all_pairs
+                .iter()
+                .map(|(a, b)| format!("{a}, {b}"))
+                .join("\n");
 
-            ui.return_button(app, |_| {});
-            ui.add_space(5.0);
-
-            ui.heading("Paired Choice");
-            ui.label("Separate conditions with commas. All ordered pairs of conditions will be produced automatically.");
-            if ui
-                .text_edit_multiline(&mut app.preference_assessment.conditions_string)
-                .changed()
-            {
-                app.preference_assessment.update_conditions();
-                app.preference_assessment.update_pairs(&mut app.rng);
+            if let Err(e) = overwrite_file(Ok(path), &data) {
+                windows_error_dialog(e)
             }
-            ui.add_space(5.0);
+        }
 
-            ui.label(format!(
-                "{}",
-                app.preference_assessment
-                    .all_pairs
-                    .iter()
-                    .map(|(a, b)| format!("[{a}, {b}]"))
-                    .join("\n")
-            ))
+        egui::CentralPanel::default().show(ui, |ui| {
+            ui.add_space(10.0);
+            ui.horizontal(|ui| {
+                ui.vertical(|ui| {
+                    ui.horizontal(|ui| {
+                        ui.heading("Paired Choice");
+                        if ui.button("Shuffle").clicked() {
+                            app.preference_assessment.shuffle_pairs(&mut app.rng);
+                        }
+                        if ui.button("Export").clicked() {
+                            app.preference_assessment.file_dialog.save_file();
+                        }
+                    });
+                    ui.label("Put each condition on a new line.");
+                    if ui
+                        .add(
+                            egui::TextEdit::multiline(
+                                &mut app.preference_assessment.conditions_string,
+                            )
+                            .hint_text(RichText::from("Condition 1\nCondition 2\nCondition 3")),
+                        )
+                        .changed()
+                    {
+                        app.preference_assessment.update();
+                    }
+                    ui.add_space(5.0);
+
+                    ui.label(format!(
+                        "With {} conditions there are {} pairs.",
+                        app.preference_assessment.conditions.len(),
+                        app.preference_assessment.all_pairs.len()
+                    ));
+                    ui.add_space(10.0);
+
+                    ui.return_button(app, |_| {});
+                });
+
+                ui.vertical(|ui| {
+                    egui::ScrollArea::vertical()
+                        .id_salt("paired choice scroller")
+                        .min_scrolled_height(550.0)
+                        .show(ui, |ui| {
+                            egui_extras::TableBuilder::new(ui)
+                                .id_salt("frequency")
+                                .column(Column::exact(125.0))
+                                .column(Column::exact(125.0))
+                                .striped(true)
+                                .body(|mut body| {
+                                    for (a, b) in app.preference_assessment.all_pairs.iter() {
+                                        body.row(20.0, |mut row| {
+                                            row.col(|ui| {
+                                                ui.label(a);
+                                            });
+                                            row.col(|ui| {
+                                                ui.label(b);
+                                            });
+                                        });
+                                    }
+                                });
+                        });
+                })
+            });
         });
     }
 }
