fd1fe1187a090fb3683f5bcc64f0c7e8aeb9ea5a
SymmetricChaos <symmetricfractal@gmail.com>
2026-08-12 14:53:40 -0400

    Update pa_page.rs

--- src/preference_assessment/pa_page.rs
+++ src/preference_assessment/pa_page.rs
@@ -1,7 +1,6 @@
-use std::fmt::Display;
-
-use crate::app::DataPro;
+use crate::{app::DataPro, ui_elements::DataProUiElements};
 use egui::Ui;
+use std::fmt::Display;
 
 #[derive(Debug, PartialEq, Eq, Clone, Default)]
 pub enum PaType {
@@ -26,15 +25,22 @@ pub struct PreferenceAssessment {
 
 impl PreferenceAssessment {
     pub fn view(app: &mut DataPro, ui: &mut Ui) {
-        egui::ComboBox::from_id_salt("preference_assessment")
-            .selected_text(app.preference_assessment.patype.to_string())
-            .show_ui(ui, |ui| {
-                ui.selectable_value(&mut app.preference_assessment.patype, PaType::None, "None");
-                ui.selectable_value(
-                    &mut app.preference_assessment.patype,
-                    PaType::PairedChoice,
-                    "Paired Choice",
-                );
-            });
+        egui::CentralPanel::default().show(ui, |ui| {
+            egui::ComboBox::from_id_salt("preference_assessment")
+                .selected_text(app.preference_assessment.patype.to_string())
+                .show_ui(ui, |ui| {
+                    ui.selectable_value(
+                        &mut app.preference_assessment.patype,
+                        PaType::None,
+                        "None",
+                    );
+                    ui.selectable_value(
+                        &mut app.preference_assessment.patype,
+                        PaType::PairedChoice,
+                        "Paired Choice",
+                    );
+                });
+            ui.return_button(app, |_| {});
+        });
     }
 }
